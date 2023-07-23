57f524f5705aa915f4758de7a55ac81ac198b99e
Doug Leinen <doug@geletkaplus.com>
2023-07-22 23:20:05 -0500

    well well well

--- gatsby-browser.jsx
+++ gatsby-browser.jsx
@@ -1,6 +1,7 @@
 import './src/styles/global.css';
 
 import React from 'react';
+import { GatsbyBrowser } from 'gatsby';
 import { AuthProvider } from './src/auth/AuthProvider';
 
 export const wrapRootElement = ({ element }) => {
